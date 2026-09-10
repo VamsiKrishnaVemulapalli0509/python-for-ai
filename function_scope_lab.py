e9c1deeee8b56c7f780a4d894e12151c6bff2bb7
VamsiKrishnaVemulapalli0509 <vamsikrishnavemulapalli1998@gmail.com>
2026-09-11 02:38:15 +0530

    Initial commit

--- function_scope_lab.py
+++ function_scope_lab.py
@@ -18,15 +18,12 @@ def greet():
 
 greet()  # Function is called properly
 
-def say_hello()   #  Missing colon
-    print("Hello!")
 
 def say_hello():  #  Colon added
     print("Hello!")
 
 
-def add_numbers(a, b):
-print(a + b)   #  Not indented inside function
+
 
 
 def add_numbers(a, b):
